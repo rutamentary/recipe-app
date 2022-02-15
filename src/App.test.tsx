b96117c114d3aa0fcf404864f4e3c19d5560609f
rutamentary <missrutamentary@gmail.com>
2022-02-15 13:08:36 -0500

    initial commit

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,25 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders search button', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const btn = screen.getByText(/Call Api/i);
+  expect(btn).toBeInTheDocument();
+});
+
+test('button does things', async () => {
+  const expectedText = 'foo'
+
+  render(<App />);
+  const textBefore = screen.queryByText(expectedText)
+  expect(textBefore).not.toBeInTheDocument()
+
+  const btn = screen.getByText(/Call Api/i);
+  fireEvent.click(btn)
+
+
+  const textAfter = await screen.findByText(expectedText)
+
+  expect(textAfter).toBeInTheDocument()
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const apiCall = async function () {
+    return 'foo';
+}
+
 function App() {
+  const [recipe, setRecipe] = useState('');
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={async function () {
+          const text = await apiCall();
+          setRecipe(text);
+      }}>Call Api</button>
+
+        <p>{recipe}</p>
     </div>
   );
 }
